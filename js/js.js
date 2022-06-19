c63c49155148380ff48e652d1694bef94551f856
Kostya-Romanovskyi <106028051+Kostya-Romanovskyi@users.noreply.github.com>
2022-06-19 15:26:10 +0300

    Add files via upload

---
+++ js/js.js
@@ -0,0 +1,50 @@
+let motto = 'What Is Dead May Never Die!';
+console.log(motto);
+let name = 'Braina';
+name = "aniarB";
+console.log(name);
+summMyBroth = 2;
+console.log(summMyBroth);
+
+let family = 'Targaryen';
+let pet = "Dragon";
+
+console.log(family);
+console.log('and');
+console.log(pet);
+
+
+console.log(Math.round(Math.random() * 10));
+console.log(typeof 'lol');
+console.log(typeof 4);
+console.log(typeof 3);
+
+let eurosCount = 100;
+let EuroPerDollar = 1.25;
+let dollarPerRuble = 60;
+
+dollarCount = eurosCount * EuroPerDollar;
+
+console.log(dollarCount);
+
+rubleCount = dollarCount * dollarPerRuble;
+console.log(rubleCount);
+
+const first = 'idduhciushcdi';
+const second = first.length;
+console.log(second);
+
+const text = 'Never forget what you are, for surely the world will not';
+console.log(`First: ${text[0]}\nLast: ${text[text.length - 1]}`);
+
+const namqe = 'Tirion';
+console.log(namqe.length.toString());
+
+const namen = 'Tirion';
+console.log(namen.toUpperCase().toLowerCase());
+
+const namey = '232323';
+console.log(namey.toString());
+
+const str = 'When \t\n you play a \t\n game of thrones you win or you die.';
+console.log(str.slice(3, 15).trim());
